04b2fb52c0a1471b16248bd753545554c9c15831
Mathias Djärv <mathias.djarv@gmail.com>
2026-03-23 13:54:57 +0100

    Move new project button to title bar as icon with tooltip

--- frontend/src/components/layout/AppSidebar.tsx
+++ frontend/src/components/layout/AppSidebar.tsx
@@ -1,18 +1,23 @@
 import { ConnectionIndicator } from "~/components/layout/ConnectionIndicator";
 import { NewProjectDialog } from "~/components/layout/NewProjectDialog";
 import { ProjectList } from "~/components/layout/ProjectList";
+import { TooltipProvider } from "~/components/ui/tooltip";
 
 export function AppSidebar() {
-	return (
-		<div className="w-64 border-r bg-muted/30 flex flex-col h-full">
-			<div className="p-4 font-semibold text-lg border-b">Agentique</div>
-			<div className="flex-1 overflow-y-auto">
-				<ProjectList />
-			</div>
-			<div className="p-3 border-t space-y-2">
-				<ConnectionIndicator />
-				<NewProjectDialog />
-			</div>
-		</div>
-	);
+  return (
+    <TooltipProvider>
+      <div className="w-64 border-r bg-muted/30 flex flex-col h-full">
+        <div className="p-4 font-semibold text-lg border-b flex items-center justify-between">
+          Agentique
+          <NewProjectDialog />
+        </div>
+        <div className="flex-1 overflow-y-auto">
+          <ProjectList />
+        </div>
+        <div className="p-3 border-t">
+          <ConnectionIndicator />
+        </div>
+      </div>
+    </TooltipProvider>
+  );
 }
